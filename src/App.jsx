1ffc073413327fa84a87532c699a86c632c1368e
luq-mne <luqman.yazrie@outlook.com>
2024-12-20 04:42:51 +0800

    uhh well

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ export default function App() {
   return (
     <div>
       <header
-      class="p-2 bg-gray-500 text-xl items-center flex flex-col fixed z-10 w-full">
-          <h1 class="text-white">
+      class="p-2 bg-white text-xl items-center flex flex-col fixed z-10 w-full">
+          <h1 class="text-black">
             bille
           </h1>
       </header>
+
+      <div class="pt-16 p-2 md:p-1 md:pt-16 lg:w-8/12 items-center mx-auto space-y-3 text-sm lg:text-base">
+        <h1 class="text-white text-2xl">
+          Well
+        </h1>
+      </div>
     </div>
   )
 }
