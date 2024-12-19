aee7e2b59cfed3f06f34e49f8a7705886e04f043
luq-mne <luqman.yazrie@outlook.com>
2024-12-20 05:15:43 +0800

    button to change between calculator and view rates

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@
 export default function App() {
   return (
     <div>
-      {/* Header */ }
+      {/* header */ }
       <header
-      class="p-2 bg-white items-center flex flex-col fixed z-10 w-full">
+      class="p-2 bg-white items-center flex flex-col fixed z-10 w-full hover:bg-[rgb(225,225,225)] duration-500">
           <h1 class="text-xl text-black">
             bille
           </h1>
       </header>
 
-      {/* Content */ }
+      {/* content */ }
       <div class="pt-16 p-4 md:p-1 md:pt-16 lg:w-8/12 items-center mx-auto space-y-3 text-sm lg:text-base">
         <div class="pb-16">
           <h1 class="text-white text-2xl">
@@ -22,32 +22,48 @@ export default function App() {
           </p>
         </div>
 
-        <div class="p-4 bg-[rgb(25,25,25)]">
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div class="flex flex-col gap-4">
+        {/* country and currency selection */ }
+        <div>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div class="flex flex-col gap-2">
               <h2 class="text-xl text-white">
                 Country
               </h2>
 
-              <select class="w-full p-2 bg-[rgb(50,50,50)] text-white">
+              <select class="w-full p-2 rounded-md text-black bg-white hover:bg-[rgb(225,225,225)] duration-300">
                 <option value="malaysia">Malaysia</option>
               </select>
             </div>
             
-            <div class="flex flex-col gap-4">
+            <div class="flex flex-col gap-2">
               <h2 class="text-xl text-white">
                 Currency
               </h2>
 
-              <select class="w-full p-2 bg-[rgb(50,50,50)] text-white">
+              <select class="w-full p-2 rounded-md text-black bg-white hover:bg-[rgb(225,225,225)] duration-300">
                 <option value="myr">MYR</option>
               </select>
             </div>
           </div>
         </div>
+
+        <hr className="border-t-2 my-4 border-[rgb(50,50,50)]" />
+
+        {/* usage */ }
+        <div class="p-2 bg-[rgb(225,225,225)] rounded-md">
+          <div class="flex flex-row gap-2">
+            <button class="p-2 bg-black rounded-md text-white">
+              Calculator
+            </button>
+
+            <button class="p-2 bg-black rounded-md text-white">
+              Rates
+            </button>
+          </div>
+        </div>
       </div>
 
-      {/* Footer */ }
+      {/* footer */ }
     </div>
   )
 }
