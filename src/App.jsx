fa61df79d4f0d60523fc057d245fc15444cd607f
luq-mne <luqman.yazrie@outlook.com>
2024-12-20 04:34:11 +0800

    color, bg change, bla bla bla

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+// SolidJS
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <header
+      class="p-2 bg-gray-500 text-xl items-center flex flex-col fixed z-10 w-full">
+          <h1 class="text-white">
+            bille
+          </h1>
+      </header>
     </div>
   )
 }
